f17911d4324d6c405d3afe869e33b0fb1e1413eb
maniksheikh <dev.maniksheikh@gmail.com>
2024-02-22 23:03:26 +0600

    create the another feture section ancd update navbar

--- pages/index.vue
+++ pages/index.vue
@@ -21,13 +21,18 @@
           <div class="w-[50%] px-20">
             <h2 class="font-bold text-4xl w-[400px] text-[#91969a] py-2">
               Creative Multipurpose <br />
-              Products <span class="text-[#da5f5c]">Template.</span> 
+              Products <span class="text-[#da5f5c]">Template.</span>
             </h2>
             <p class="text-gray-500 leading-6 mt-2 py-1 w-[470px] font-bold">
-              This template is specially designed for a Products company,  <br> that
-              want`s something simple but professionaly.
+              This template is specially designed for a Products company, <br />
+              that want`s something simple but professionaly.
             </p>
-            <button class="items-center justify-center m-auto bg-blue-500 text-white px-6 py-2 rounded-md font-bold mt-4 hover:bg-sky-700"  type="button">Get Started</button>
+            <button
+              class="items-center justify-center m-auto bg-blue-500 text-white px-6 py-2 rounded-md font-bold mt-4 hover:bg-sky-700"
+              type="button"
+            >
+              Get Started
+            </button>
           </div>
           <img
             class="w-[50%] h-[450px] cursor-pointer"
@@ -35,6 +40,84 @@
           />
         </div>
       </section>
+      
+
+      <feture>
+        <div>
+        <div class="bg-[#ffffff] font-sans h-[100px] flex items-center justify-between  w-[60%] m-auto mt-20">
+          <div class="flex text-center items-center justify-center w-[400px] py-6 border-r-2 border-gray-200 ">
+            <svg class="bg-red-100 rounded-full p-1"
+              xmlns="http://www.w3.org/2000/svg"
+              width="2rem"
+              height="2rem"
+              viewBox="0 0 24 24"
+            >
+              <g fill="none" fill-rule="evenodd">
+                <path
+                  d="M24 0v24H0V0zM12.594 23.258l-.012.002l-.071.035l-.02.004l-.014-.004l-.071-.036c-.01-.003-.019 0-.024.006l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427c-.002-.01-.009-.017-.016-.018m.264-.113l-.014.002l-.184.093l-.01.01l-.003.011l.018.43l.005.012l.008.008l.201.092c.012.004.023 0 .029-.008l.004-.014l-.034-.614c-.003-.012-.01-.02-.02-.022m-.715.002a.023.023 0 0 0-.027.006l-.006.014l-.034.614c0 .012.007.02.017.024l.015-.002l.201-.093l.01-.008l.003-.011l.018-.43l-.003-.012l-.01-.01z"
+                />
+                <path
+                  fill="black"
+                  d="M12 2C6.477 2 2 6.477 2 12s4.477 10 10 10s10-4.477 10-10S17.523 2 12 2M8.5 9.5a3.5 3.5 0 1 1 7 0a3.5 3.5 0 0 1-7 0m9.758 7.484A7.985 7.985 0 0 1 12 20a7.985 7.985 0 0 1-6.258-3.016C7.363 15.821 9.575 15 12 15s4.637.821 6.258 1.984"
+                />
+              </g>
+            </svg>
+            <div class="px-4">
+              <span class="font-bold text-xl">390+</span>
+              <p class="text-gray-700 text-md">Users</p>
+            </div>
+          </div>
+          <div class="flex text-center items-center justify-center w-[400px] py-6 border-r-2 border-gray-200">
+            <svg class="bg-red-100 rounded-full p-1"
+              xmlns="http://www.w3.org/2000/svg"
+              width="2rem"
+              height="2rem"
+              viewBox="0 0 512 512"
+            >
+              <path
+                fill="none"
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="32"
+                d="M256 48c-79.5 0-144 61.39-144 137c0 87 96 224.87 131.25 272.49a15.77 15.77 0 0 0 25.5 0C304 409.89 400 272.07 400 185c0-75.61-64.5-137-144-137"
+              />
+              <circle
+                cx="256"
+                cy="192"
+                r="48"
+                fill="none"
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="32"
+              />
+            </svg>
+            <div class="px-4">
+              <span class="font-bold text-xl">20+</span>
+              <p class="text-gray-700 text-md">Locations</p>
+            </div>
+          </div>
+          <div class="flex text-center items-center justify-center w-[400px] py-6">
+            <svg class="bg-red-100 rounded-full p-1"
+              xmlns="http://www.w3.org/2000/svg"
+              width="2rem"
+              height="2rem"
+              viewBox="0 0 24 24"
+            >
+              <path
+                fill="currentColor"
+                d="M2 4.6v4.8c0 .9.5 1.6 1.2 1.6h17.7c.6 0 1.2-.7 1.2-1.6V4.6C22 3.7 21.5 3 20.8 3H3.2C2.5 3 2 3.7 2 4.6M10 8V6H9v2zM5 8h2V6H5zm15 1H4V5h16zM2 14.6v4.8c0 .9.5 1.6 1.2 1.6h17.7c.6 0 1.2-.7 1.2-1.6v-4.8c0-.9-.5-1.6-1.2-1.6H3.2c-.7 0-1.2.7-1.2 1.6m8 3.4v-2H9v2zm-5 0h2v-2H5zm15 1H4v-4h16z"
+              />
+            </svg>
+            <div class="px-4">
+              <span class="font-bold text-xl">50+</span>
+              <p class="text-gray-700 text-md">Servar</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      </feture>
     </div>
   </div>
 </template>
