52294cf47d2595c79740e04b73a8909d7c9445ea
maniksheikh <dev.maniksheikh@gmail.com>
2024-09-24 11:24:59 +0600

    update package

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
   devtools: { enabled: false },
 
   modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt", "nuxt-swiper"],
+
+  vite: {
+    assetsInclude: ["**/*.JPG"],
+  },
 });
